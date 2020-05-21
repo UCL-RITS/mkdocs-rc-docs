8262cc47900d64bf913752141837d4f1b7a577ea
Ian Kirker <ian.kirker@gmail.com>
2020-05-21 13:17:41 +0100

    Refactors and improves error messages in package list gen

--- builder/convert_lists.py
+++ builder/convert_lists.py
@@ -5,36 +5,70 @@ import re
 import requests
 import mako
 import os
+import sys
 import datetime
 from mako.template import Template
 from mako.lookup import TemplateLookup
 from mako.exceptions import RichTraceback
+from collections import namedtuple
 
+PackagePageFactory = namedtuple("PackagePage", ["datafile", "template", "output"])
 
-use_local_sources = False
+pages_to_render = [
+    PackagePageFactory(
+        datafile = "r-packages.json",
+        template = "r_description.md.mako",
+        output =   "r-packages.md",
+        ),
+    PackagePageFactory(
+        datafile = "python-packages.json",
+        template = "python_description.md.mako",
+        output =   "python-packages.md",
+        ),
+    PackagePageFactory(
+        datafile = "module-packages.json",
+        template = "modules_template.md.mako",
+        output =   "module-packages.md",
+        ),
+    ]
 
-if use_local_sources:
-    r_pack_file = open("r-packages.json")
-    p_pack_file = open("python-packages.json")
-    m_pack_file = open("module-packages.json")
+url_stem = "https://www.rc.ucl.ac.uk/lists/"
 
-    r_pack = json.loads(r_pack_file.read())
-    p_pack = json.loads(p_pack_file.read())
-    m_pack = json.loads(m_pack_file.read())
-else:
-    url_stem = "https://www.rc.ucl.ac.uk/lists/"
-    r_pack = requests.get(url_stem + "r-packages.json").json()
-    p_pack = requests.get(url_stem + "python-packages.json").json()
-    m_pack = requests.get(url_stem + "module-packages.json").json()
+def render_page(page_info, use_local_sources=False):
+    try:
+        if use_local_sources:
+            package_file = open(page_info.datafile)
+            package_dict = json.loads(package_file.read())
+        else:
+            package_dict = requests.get(url_stem + page_info.datafile).json()
+    except:
+        sys.stderr.write("Error reading package JSON info from \"%s\".\n" % page_info.datafile)
+        raise
+
+    package_dict["generated"] = make_friendlier_datetime(package_dict["generated"])
+
+    page_template = get_mako_template(page_info.template)
+
+    # Exception code from: https://docs.makotemplates.org/en/latest/usage.html#handling-exceptions
+    try:
+        with open(page_info.output, "w") as f:
+            f.write(postproc(page_template.render(p=package_dict)))
+    except:
+        traceback = RichTraceback()
+        for (filename, lineno, function, line) in traceback.traceback:
+            print("File %s, line %s, in %s" % (filename, lineno, function))
+            print(line, "\n")
+        print("%s: %s" % (str(traceback.error.__class__.__name__), traceback.error))
+        raise
+
+def get_mako_template(name):
+    template_dirs = TemplateLookup(directories=[os.environ['MAKO_TEMPLATE_DIR'],"./",'./templates'], preprocessor=preprocs)
+    t = template_dirs.get_template(uri="/" + name)
+    return t
 
 def make_friendlier_datetime(s):
     dt = datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S%z")
     return dt.strftime("%H:%M:%S (%z) on %m %b %Y")
-
-r_pack["generated"] = make_friendlier_datetime(r_pack["generated"])
-p_pack["generated"] = make_friendlier_datetime(p_pack["generated"])
-m_pack["generated"] = make_friendlier_datetime(m_pack["generated"])
-
 
 # I *would* manage to pick a templating library that treats ## as a comment C_C
 # Fortunately you can pass a pre-processing function to handle it
@@ -51,23 +85,10 @@ def guard_comments(s):
     s = re.sub("^([ \\t]*)(##+) ", "\\1${'\\2'} ", s, flags=re.MULTILINE)
     return s
 
-template_dirs = TemplateLookup(directories=[os.environ['MAKO_TEMPLATE_DIR'],"./",'./templates'], preprocessor=preprocs)
-
-r_t = template_dirs.get_template(uri="/r_description.md.mako")
-p_t = template_dirs.get_template(uri="/python_description.md.mako")
-m_t = template_dirs.get_template(uri="/modules_template.md.mako")
-
-# Exception code from: https://docs.makotemplates.org/en/latest/usage.html#handling-exceptions
-try:
-    with open("r-packages.md", "w") as f:
-        f.write(postproc(r_t.render(p=r_pack)))
-    with open("python-packages.md", "w") as f:
-        f.write(postproc(p_t.render(p=p_pack)))
-    with open("module-packages.md", "w") as f:
-        f.write(postproc(m_t.render(m=m_pack)))
-except:
-    traceback = RichTraceback()
-    for (filename, lineno, function, line) in traceback.traceback:
-        print("File %s, line %s, in %s" % (filename, lineno, function))
-        print(line, "\n")
-    print("%s: %s" % (str(traceback.error.__class__.__name__), traceback.error))
+if __name__ == "__main__":
+    for p in pages_to_render:
+        try:
+            render_page(p, use_local_sources=False)
+        except:
+            sys.stderr.write("Error creating page \"%s\". See traceback for details.\n" % p.output)
+            raise
